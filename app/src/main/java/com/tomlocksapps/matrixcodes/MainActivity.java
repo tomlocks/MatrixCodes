774c43910bbc8456f0f0c75a1ad2ca8ec83a19e7
tomlocks <tomlocks@gmail.com>
2015-03-17 21:12:05 +0100

    Camera Image to Mat coversion DONE

--- app/src/main/java/com/tomlocksapps/matrixcodes/MainActivity.java
+++ app/src/main/java/com/tomlocksapps/matrixcodes/MainActivity.java
@@ -2,14 +2,21 @@ package com.tomlocksapps.matrixcodes;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.ImageFormat;
 import android.hardware.Camera;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.highgui.Highgui;
 
 
 public class MainActivity extends Activity {
@@ -17,6 +24,7 @@ public class MainActivity extends Activity {
 
     private FrameLayout preview;
     private CameraHelper cameraHelper;
+    private ImageView imageViewPreview;
 
     private Camera.PictureCallback pictureCallback = new Camera.PictureCallback() {
         @Override
@@ -24,6 +32,33 @@ public class MainActivity extends Activity {
 
             if (((MatrixCodesApplication) getApplication()).isOpenCVLoaded()) {
                 // TODO add code for finding, recognizing and calculating distance.
+
+                int format = camera.getParameters().getPictureFormat();
+
+                Camera.Size size = camera.getParameters().getPictureSize();
+
+                long start = System.currentTimeMillis();
+
+                if(format == ImageFormat.JPEG) {
+                    Mat m = new Mat( 1 , data.length , CvType.CV_8UC1);
+                    m.put(0,0,data);
+                    Log.d("Mat", "rows: " + m.rows() + " ;cols: " + m.cols());
+                    Mat bgrMat = new Mat();
+                    bgrMat = Highgui.imdecode(m, Highgui.IMREAD_GRAYSCALE);
+                    Log.d("bgrMat", "rows: " + bgrMat.rows() + " ;cols: " + bgrMat.cols() + "; type: " + bgrMat.type());
+                    Bitmap bmp = Bitmap.createBitmap(bgrMat.cols(), bgrMat.rows(), Bitmap.Config.ARGB_8888);
+                    Utils.matToBitmap(bgrMat, bmp);
+                    Log.d("start: ",   System.currentTimeMillis()- start + "");
+
+
+
+                    imageViewPreview.setImageBitmap(bmp);
+
+
+                }
+
+
+
             }
 
             camera.startPreview();
@@ -61,8 +96,10 @@ public class MainActivity extends Activity {
 
         cameraHelper = new CameraHelper(this, preview, pictureCallback);
 
+        imageViewPreview = (ImageView) findViewById(R.id.imageViewPreview);
+
         // TODO - check the length of supported picture sizes!
-        cameraHelper.setPictureSize(cameraHelper.getParameters().getSupportedPictureSizes().get(5));
+        cameraHelper.setPictureSize(cameraHelper.getParameters().getSupportedPictureSizes().get(8));
 
     }
 
