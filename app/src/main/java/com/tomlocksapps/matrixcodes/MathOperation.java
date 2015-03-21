56e4493bd07ac9fb37aa49dc0c97736b3710f261
AveAmore <sebastian.drogowski@gmail.com>
2015-03-22 00:53:07 +0100

    MathFunction
    Degree count

--- app/src/main/java/com/tomlocksapps/matrixcodes/MathOperation.java
+++ app/src/main/java/com/tomlocksapps/matrixcodes/MathOperation.java
@@ -2,7 +2,9 @@ package com.tomlocksapps.matrixcodes;
 
 import android.app.Application;
 import android.graphics.Point;
+import android.graphics.PointF;
 
+import static java.lang.Math.*;
 /**
  * Created by Seba on 2015-03-03.
  */
@@ -12,20 +14,41 @@ public class MathOperation extends Application {
 
     /**
      *
-     * @param vect1 first point of QRcode
-     * @param vect2 center point of QRcode
-     * @param vect3 third point of QRcode
-     * @param degr1 degree around x axis
-     * @param degr2 degree around z axis
+     * @param point1 first point of QRcode
+     * @param point2 center point of QRcode
+     * @param point3 third point of QRcode
+     * @param xdeg degree around x axis
+     * @param zdeg degree around z axis
      * @return
      */
-public int DegreeMath(Point vect1, Point vect2, Point vect3, float degr1, float degr2)
+public float DegreeMath(Point point1, Point point2, Point point3, float xdeg, float zdeg)
 {
 
+   PointF vect1 = new PointF();
+   PointF vect2 = new PointF();
+   PointF vect1_temp = new PointF();
+   PointF vect2_temp = new PointF();
+   float vertical,horizontal;
+   float DegreeParam;
 
+//vect declaration
+    vect1.x = point1.x-point2.x;
+    vect1.y = (float) ((point1.y-point2.y)/cos(xdeg));
+    vect2.x = point3.x-point2.x;
+    vect2.y = (float)((point3.y-point2.y)/cos(xdeg));
+    vect1_temp = vect1;
+    vect2_temp = vect2;
+    vect1.x = (float)(vect1_temp.x * cos(zdeg)- vect1_temp.y * sin(zdeg));
+    vect1.y = (float)(vect1_temp.x * sin(zdeg) + vect1_temp.y * cos(zdeg));
 
+    vect2.x = (float)(vect2_temp.x * cos(zdeg) - vect2_temp.y * sin(zdeg));
+    vect2.y = (float)(vect2_temp.x * sin(zdeg) + vect2_temp.y * cos(zdeg));
 
-return 0;
+    vertical = (float)(sqrt(vect1.x*vect1.x+vect1.y*vect1.y));
+    horizontal = (float)(sqrt(vect2.x*vect2.x+vect2.y*vect2.y));
+    DegreeParam = (float)(acos(horizontal/vertical));
+
+    return DegreeParam;
 }
 
 }
