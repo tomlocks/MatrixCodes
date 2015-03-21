81f62286efa6b88b19800705d2d3d1889afdec7b
AveAmore <sebastian.drogowski@gmail.com>
2015-03-21 11:48:19 +0100

    MathOperation
    added math class to program

---
+++ app/src/main/java/com/tomlocksapps/matrixcodes/MathOperation.java
@@ -0,0 +1,31 @@
+package com.tomlocksapps.matrixcodes;
+
+import android.app.Application;
+import android.graphics.Point;
+
+/**
+ * Created by Seba on 2015-03-03.
+ */
+public class MathOperation extends Application {
+    int x;
+    int y;
+
+    /**
+     *
+     * @param vect1 first point of QRcode
+     * @param vect2 center point of QRcode
+     * @param vect3 third point of QRcode
+     * @param degr1 degree around x axis
+     * @param degr2 degree around z axis
+     * @return
+     */
+public int DegreeMath(Point vect1, Point vect2, Point vect3, float degr1, float degr2)
+{
+
+
+
+
+return 0;
+}
+
+}
